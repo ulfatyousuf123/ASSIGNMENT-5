ab28933cf89c3c596fd6389903302a152c0598b6
ulfatyousuf <snow10515@gmail.com>
2023-09-26 06:46:39 -0700

    Add assignment

---
+++ app (2).js
@@ -0,0 +1,269 @@
+//****************************chap # 21 *************************************
+//Q1......
+
+
+//  var city = prompt("Enter your city");
+//  city = city.toLowerCase();
+//  var Cities = ["karachi", "islamabad", "faislabad", "lahore", "ravalpindii"];
+//  for (var i = 0; i <= 4; i++) {
+//  if (city === Cities[i]) {
+//  alert("It's one of the  cities");
+//  }
+//  console.log(city);
+//  }
+
+
+//Q2.....
+
+// var text = prompt ("ENTER A WORD X");
+// var result = text.toLowerCase();
+// console.log(result);
+
+//Q3......
+
+// var text = prompt ("ENTER A WORD Y");
+// var result = text.toUpperCase();
+// console.log(result);
+
+
+//Q4.......
+
+
+// var text = "hello world!";
+// var result = text.toLowerCase();
+// console.log(result);
+
+
+//Q5.......
+
+// var fruits = prompt("Enter a fruit name");
+// var remainChar = fruits.slice ();
+// remainChar = remainChar.toLowerCase();
+
+// var arr = ["APPLE","MANGO","BANANA","GRAPES"];
+// for ( var i = 0 ; i<arr.length ; i++);{
+
+
+// if (arr[i] === fruits);
+
+// }
+
+// console.log(remainChar);
+
+
+//Q6.....
+
+
+// var text = prompt ("Are you learn javascript");
+ 
+// text = text.toUpperCase();
+// console.log(text);
+
+// if (text === 'yes') {
+//   alert('welcome to javascript!');
+// } 
+
+
+//Q7.......
+
+
+
+// var cityName = prompt ("Enter where to you live? ");
+// console.log(cityName);
+// var firstChar = cityName.slice(0,1).toUpperCase();
+// var remainChar = cityName.slice(1).toLowerCase();
+
+// var city = firstChar + remainChar;
+// console.log(city);
+
+// var cityArr = [ "Karachi"];
+
+// for( var i = 0; i < cityArr.length ; i++){
+//     if ( city === cityArr[i]){
+//         alert("city found");
+//     }
+// } 
+
+// *************************CHAP # 22,25*****************************
+//Q1......
+
+// var str = "captain"
+// var indexNumber = str.indexOf("ap");
+
+// var firstText = str.slice(0, indexNumber);
+// var replaceWord = "ur";
+// var remainText = str.slice(indexNumber + 2);
+
+// console.log(firstText + replaceWord + remainText);
+
+
+//Q3......
+
+
+// var animal = "elephant";
+// var indexNumber = animal.indexOf("epha");
+
+// var firstText = animal.slice(0,indexNumber);
+// var replaceWord = "seg";
+// var remainText = animal.slice(indexNumber + 3);
+// console.log(firstText + replaceWord + remainText);
+
+
+
+//Q6.......
+
+
+// var text = "To be or not to be.";
+// var indexNumber = text.indexOf("be");
+//  console.log (indexNumber);
+
+//Q7.........
+
+
+// var text = "To be or not to be.";
+// var indexNumber = text.lastIndexOf("be");
+//  console.log( indexNumber);
+
+
+//Q10.....
+
+// var str = "abcde";
+// console.log(str.charAt(2));
+
+
+
+//Q11......
+ 
+
+// var str = "10th character";
+// console.log(str.charAt(5),str.charAt(6),str.charAt(7));
+
+
+//Q16....
+
+// var str = "1";
+// var indexNumber = str.indexOf("one");
+// var firstText = str.slice(0,indexNumber);
+// var replaceWord = "one";
+// var remainText = str.slice(indexNumber , 0);
+// console.log(firstText + replaceWord + remainText);
+
+
+//Q17......
+
+// var str = "y";
+// var replaceWord = "," + "a"+ "," + "z";
+// console.log(str + replaceWord);
+
+
+//*******************************chap 26***************************
+
+//Q1.......
+
+    // var num = .000001;
+    // console.log(Math.ceil(num));
+
+ //Q2.....
+
+    // var num = 2.49;
+    // console.log(Math.floor(num));
+
+//Q3.......
+
+
+// var num = 4.49;
+//      console.log(Math.floor(num));
+
+//Q4......
+
+// var num = 3.49;
+//  console.log(Math.ceil(num));
+
+
+
+//Q5.......
+
+// var Numbers = Math.round(.5) * 0;
+// console.log(Numbers) ;
+
+//**************************CHAP 27  */
+
+//Q1.....
+
+
+// var randomNumbers = Math.random() * 50;
+// console.log(randomNumbers.toFixed(3) ) ;
+
+//Q2.....
+
+// var  randomNumbers = Math.random() * 20;
+// console.log(randomNumbers.toFixed(1));
+
+//Q3.......
+// Math.floor( Math.random() * 6 );
+// console.log(Math.floor);
+
+// var dice = Math.floor( Math.random() * 6 ) +1;
+// console.log(dice);
+
+
+//***************************chap 28 , 29**************** */
+ //Q1.....
+
+//  var num1 = parseInt(prompt("Enter your value"));
+//  var num2 = parseInt(prompt("Enter your value"));
+// var add = num1 +num2;
+//  console.log(add);
+
+//  Q4......
+
+// var num = 4.59;
+// console.log(Math.random(num));
+
+
+
+    //Q5.......
+
+// var num1 = Number(prompt("Enter your value"));
+// var num2 = Number(prompt("Enter your value"));
+// var add = num1 +num2;
+// console.log(typeof add.toString());
+
+
+//Q6.....
+
+// var num = 42;
+// console.log(typeof num.toString());
+
+//Q7......
+
+// var num = "3.14"
+// console.log(Math.round(num));
+
+
+//*******************chap # 30***********************
+//Q1......
+// var num1 = 3.241;
+// var num2 = 2.543;
+
+// var newnum = num1+ num2;
+// console.log( Math.round(newnum).toFixed(4));
+
+
+//Q2....
+
+// var num1 = 2.451;
+// var num2 = 4.971;
+
+// var newnum = num1+ num2;
+// console.log( Math.round(newnum).toFixed(2));
+
+
+//Q4.....
+
+// var num1 = 1.451;
+// var num2 = 5.151;
+
+// var newnum = num1+ num2;
+// console.log( Math.round(newnum).toFixed(2));
+
